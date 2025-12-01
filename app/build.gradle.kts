3f527fc02ffe231aa6a05334ab7585c2d43c9593
TheOtherUser3 <dawsonmaska@gmail.com>
2025-11-30 22:17:23 -0500

    Fixed API call to use Retrofit + Moshi.  Added button to add all missing ingredients.  Meal cards now link to recipe page on spoonacular.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,5 +84,12 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    
 }
 
